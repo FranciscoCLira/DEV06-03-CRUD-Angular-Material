870a1f2fb6bc72cbde0f52c57308fc10ae41ab22
Francisco Casemiro Lira <franciscoclira@hotmail.com>
2025-05-10 21:02:32 -0300

    28-Finalizando a parte visual do formulário de Clientes
    Adicionado codigom em
       cadastro.components.html
       cadastro.components.ts
       styless.css  ==> estilos globais

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button'; 
 
-import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-cadastro',
@@ -13,7 +16,9 @@ import { FormsModule } from '@angular/forms';
     MatCardModule,
     FormsModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
   ],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss',
