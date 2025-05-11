c608fa37368619d7d4638e44e513a48a0d7ba2d4
Francisco Casemiro Lira <franciscoclira@hotmail.com>
2025-05-10 23:33:07 -0300

    4-Criando o formulário de pesquisa - Consulta
    1. Não utilizaremos s arquivos ...spec.ts de cadastro e consulta, então vamos deletar:
      src\app\cadastro\cadastro.component.spec.ts
      src\app\cadastro\consulta.component.spec.ts
    
    2. incluir codigo em src\app\consulta\consulta.component.ts
    
    3. incluir codigo em src\app\consulta\consulta.component.html

--- src/app/consulta/consulta.component.ts
+++ src/app/consulta/consulta.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms'; 
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+
 @Component({
   selector: 'app-consulta',
-  imports: [],
+  imports: [
+    FlexLayoutModule,
+    MatCardModule,
+    FormsModule,
+    MatInputModule,
+    MatIconModule,
+    MatTableModule,
+    MatButtonModule
+  ],
   templateUrl: './consulta.component.html',
   styleUrl: './consulta.component.scss'
 })
